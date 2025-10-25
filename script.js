4973dbdabadfbf7e331fa512a941e76a3d0562fc
ayushsharma7700 <ayushmps111@gmail.com>
2025-10-25 22:23:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXX",
+  authDomain: "personality-rating.firebaseapp.com",
+  projectId: "personality-rating",
+  storageBucket: "personality-rating.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:abcdef123456"
+};
